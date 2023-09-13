e9bd1092f1653cabc6c9d4eea8c58553f0822955
Herbert Djanie <hdjaniejnr@gmail.com>
2023-09-13 15:57:44 +0000

    app updated

---
+++ src/sentiment_app.py
@@ -0,0 +1,143 @@
+
+# import streamlit as st
+# from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
+# from scipy.special import softmax
+
+# # Load your model and tokenizer
+# model_path = "petermutwiri/distilbert-base-uncased-Distilbert-Model"
+# tokenizer = AutoTokenizer.from_pretrained(model_path)
+# config = AutoConfig.from_pretrained(model_path)
+# model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+# # Preprocess text (username and link placeholders)
+# def preprocess(text):
+#     new_text = []
+#     for t in text.split(" "):
+#         t = '@user' if t.startswith('@') and len(t) > 1 else t
+#         t = 'http' if t.startswith('http') else t
+#         new_text.append(t)
+#     return " ".join(new_text)
+
+# def sentiment_analysis(text):
+#     text = preprocess(text)
+
+#     # PyTorch-based models
+#     encoded_input = tokenizer(text, return_tensors='pt')
+#     output = model(**encoded_input)
+#     scores_ = output[0][0].detach().numpy()
+#     scores_ = softmax(scores_)
+
+#     # Format output dict of scores
+#     labels = ['Negative', 'Neutral', 'Positive']
+#     scores = {l: float(s) for (l, s) in zip(labels, scores_)}
+
+#     return scores
+
+# # Streamlit app layout
+# st.title("Sentiment Analysis App")
+
+# # Initialize the sentiment_label variable
+# sentiment_label = None
+
+# # Input text area for user to enter a tweet
+# input_text = st.text_area("Write your tweet here...")
+
+# # Buttons to Analyze Sentiment and Cancel
+# col1, col2 = st.columns(2)
+# if col1.button("Analyze Sentiment"):
+#     if input_text:
+#         # Perform sentiment analysis using the loaded model
+#         scores = sentiment_analysis(input_text)
+#         st.write("Sentiment Scores:")
+#         for label, score in scores.items():
+#             st.write(f"{label}: {score:.2f}")
+
+#         # Determine the overall sentiment label
+#         sentiment_label = max(scores, key=scores.get)
+
+#         # Map sentiment labels to human-readable forms
+#         sentiment_mapping = {
+#             "Negative": "Negative",
+#             "Neutral": "Neutral",
+#             "Positive": "Positive"
+#         }
+#         sentiment_readable = sentiment_mapping.get(sentiment_label, "Unknown")
+
+#         # Display the sentiment analysis result
+#         col2.write("Sentiment:", sentiment_readable)
+
+# # Button to Cancel
+# if col1.button("Cancel"):
+#     input_text = ""  # Clear the user's input
+import streamlit as st
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
+from scipy.special import softmax
+
+# Load your model and tokenizer
+model_path = "petermutwiri/distilbert-base-uncased-Distilbert-Model"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+config = AutoConfig.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+# Preprocess text (username and link placeholders)
+#In summary, this preprocessing function helps ensure that usernames and links in the input text do not interfere with the sentiment analysis performed by the model. It replaces them with placeholder tokens to maintain the integrity of the text's structure while anonymizing or standardizing specific elements.
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+def sentiment_analysis(text):
+    text = preprocess(text)
+
+    # PyTorch-based models
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores_ = output[0][0].detach().numpy()
+    scores_ = softmax(scores_)
+
+    # Format output dict of scores
+    labels = ['Negative', 'Neutral', 'Positive']
+    scores = {l: float(s) for (l, s) in zip(labels, scores_)}
+
+    return scores
+
+# Streamlit app layout with two columns
+st.title("Sentiment Analysis App")
+st.write(" Sentiment analysis, also known as opinion mining, is the process of determining the emotional tone or sentiment expressed in text data, whether it's positive,negative, or neutral")
+st.image("Assets/sent_emoji.png", caption="Sentiments examples", use_column_width=True)
+
+# Input text area for user to enter a tweet in the left column
+input_text = st.text_area("Write your tweet here...")
+
+# Output area for displaying sentiment in the right column
+if st.button("Analyze Sentiment"):
+    if input_text:
+        # Perform sentiment analysis using the loaded model
+        scores = sentiment_analysis(input_text)
+
+        # Display sentiment scores in the right column
+        st.text("Sentiment Scores:")
+        for label, score in scores.items():
+            st.text(f"{label}: {score:.2f}")
+
+        # Determine the overall sentiment label
+        sentiment_label = max(scores, key=scores.get)
+
+        # Map sentiment labels to human-readable forms
+        sentiment_mapping = {
+            "Negative": "Negative",
+            "Neutral": "Neutral",
+            "Positive": "Positive"
+        }
+        sentiment_readable = sentiment_mapping.get(sentiment_label, "Unknown")
+
+        # Display the sentiment label in the right column
+        st.text(f"Sentiment: {sentiment_readable}")
+
+# Button to Clear the input text
+if st.button("Clear Input"):
+    input_text = ""
